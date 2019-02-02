6152b5e41267e52ac175826a859092a6b0bd00b5
Drago Todorov <todomodo@users.noreply.github.com>
2019-02-01 19:15:03 -0800

    progressing with the React implementation, also preparing the Angular implementations as well

--- 2019-react/trt-board.js
+++ 2019-react/trt-board.js
@@ -1,12 +1,24 @@
+/*
+	The game board is a table, on which shapes are displayed
+*/
 'use strict';
-
+	   
 class TrtBoard extends React.Component {
 
   render() {
-    return 'Board (' + this.props.rows + ', ' + this.props.columns + ')';    
+	let rows = [];
+    for (var row_index = 0; row_index < this.props.rows; row_index++){
+      let cells = []
+      for (var cell_index = 0; cell_index < this.props.columns; cell_index++){
+		let cellStyle = `color-null`
+        cells.push(<td class={cellStyle}></td>)
+      }
+      rows.push(<tr>{cells}</tr>)
+    }
+	return(<tbody>{rows}</tbody>)	
   }
 }
 
 ReactDOM.render(
-	React.createElement(TrtBoard, { rows: 10, columns: 20 }), 
-	document.querySelector('#TRT_BOARD'));
+	React.createElement(TrtBoard, { rows: 10, columns: 10 }), 
+	document.querySelector('#trt-board'));
